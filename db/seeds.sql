62496aa543b8f22dd2a978fc0753a36bc98bf2b9
Lou-HTX <luismoreno.it@gmail.com>
2017-06-08 10:20:52 -0500

    Database files created.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO table_name (column_name, column_name)VALUES("add_this_to_database");
+-- INSERT INTO burgers (Burger_Name)VALUES("Texas Burger");
+
+INSERT INTO burgers(burger_name) VALUES ("NOLA Burger");
+INSERT INTO burgers(burger_name) VALUES ("Chicago Burger");
+INSERT INTO burgers(burger_name) VALUES ("Houston Burger");
---
+++ server.js
@@ -0,0 +1,3 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var method = require('method-override');
