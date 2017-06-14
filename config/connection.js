03daf3f70bf637e214e183173927c83234b7196f
Lou-HTX <luismoreno.it@gmail.com>
2017-06-14 18:24:34 -0500

    db connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,24 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "test",
-    password: "Welcome01",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+        user: 'vyrgjzdxyx18mr7w',
+        password: 'l9c6lhcbkaj30mis',
+        database: 'burgers_db'
+    })
+}
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "test",
+//     password: "Welcome01",
+//     database: "burgers_db"
+// });
+
 
 connection.connect(function(err) {
     if (err) {
