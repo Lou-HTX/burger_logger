c44c4adb3fa4e9e3a4cd39c76df4e1f34640a53d
Lou-HTX <luismoreno.it@gmail.com>
2017-06-23 16:16:11 -0500

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,35 +1,57 @@
 var express = require("express");
 var burger = require("../models/burger.js");
 var router = express.Router();
+var bodyParser = require('body-parser');
+var connection = require('../config/connection.js');
+
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
-        var hbsObject = {
-            burger: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+    burger.selectAll(function(data) {
+        res.render("index", { burgers: data });
     });
 });
 
 router.post("/", function(req, res) {
-    burger.create([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function() {
+    burger.insertOne(req.body.burger_name, function() {
         res.redirect("/");
     });
 });
 
 router.put("/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition", condition);
-    burger.update({
-        devoured: req.body.devoured
-    }, condition, function() {
+    burger.updateOne(req.params.id, function() {
         res.redirect("/");
     });
 });
+
+
+// router.get("/", function(req, res) {
+//     burger.all(function(data) {
+//         var hbsObject = {
+//             burger: data
+//         };
+//         console.log(hbsObject);
+//         res.render("index", hbsObject);
+//     });
+// });
+
+// router.post("/", function(req, res) {
+//     burger.create([
+//         "burger_name", "devoured"
+//     ], [
+//         req.body.burger_name, req.body.devoured
+//     ], function() {
+//         res.redirect("/");
+//     });
+// });
+
+// router.put("/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
+//     console.log("condition", condition);
+//     burger.update({
+//         devoured: req.body.devoured
+//     }, condition, function() {
+//         res.redirect("/");
+//     });
+// });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,42 @@
 var orm = require("../config/orm.js");
+var express = require("express");
+var app = express();
 
 var burger = {
-    all: function(cb) {
-        orm.all("burgers", function(res) {
+    selectAll: function(cb) {
+        orm.selectAll('burgers', function(res) {
             cb(res);
         });
     },
-    create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+    insertOne: function(value, cb) {
+        orm.insertOne('burgers', "burger_name", value, function(res) {
             cb(res);
         });
     },
-    update: function(objColVal, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
+    updateOne: function(condition, cb) {
+        orm.updateOne('burgers', 'devoured', '1', 'id', condition, function(res) {
             cb(res);
-        });
+        })
     }
-};
+}
+
+
+// var burger = {
+//     all: function(cb) {
+//         orm.all("burgers", function(res) {
+//             cb(res);
+//         });
+//     },
+//     create: function(cols, vals, cb) {
+//         orm.create("burgers", cols, vals, function(res) {
+//             cb(res);
+//         });
+//     },
+//     update: function(objColVal, condition, cb) {
+//         orm.updateOne("burgers", objColVals, condition, function(res) {
+//             cb(res);
+//         });
+//     }
+// };
 
 module.exports = burger;
