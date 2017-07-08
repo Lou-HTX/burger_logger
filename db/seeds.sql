2778d3bb16a8f36c61ce0cb62a3ce63e8a7d94b6
Lou-HTX <luismoreno.it@gmail.com>
2017-07-08 16:59:37 -0500

    udated some routing

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
--- INSERT INTO table_name (column_name, column_name)VALUES("add_this_to_database");
--- INSERT INTO burgers (Burger_Name)VALUES("Texas Burger");
-
-INSERT INTO burgers(burger_name, devoured) VALUES ("NOLA Burger", false);
-INSERT INTO burgers(burger_name, devoured) VALUES ("Chicago Burger", false);
-INSERT INTO burgers(burger_name, devoured) VALUES ("Houston Burger", false);
--- server.js
+++ server.js
@@ -1,25 +1,53 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+var db = require("./models");
+var sequelize = require("sequelize");
+var methodOverride = require("method-override");
+// ====================================================================
+// Serve static content for the app from the "public" directory in the application directory.
+// ====================================================================
 app.use(express.static(__dirname + "/public"));
-// app.use(express.static(process.cwd() + "/public"));
-
+// ====================================================================
+//For BodyParser
+// ==================================================================== 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(methodOverride("_method"));
-
+app.use(bodyParser.json());
+// ====================================================================
+//Override with POST having ?_method=DELETE
+// ====================================================================
+app.use(methodOverride('__method'));
+// ====================================================================
+//for handlebars
+// ====================================================================
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
-
+// ====================================================================
+//Routes
+// ====================================================================
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+var routes = require("./controllers/moped_Controller.js");
 app.use("/", routes);
-
-app.listen(port);
+// ====================================================================
+//Sync Database
+// ====================================================================
+db.sequelize.sync({ force: false }).then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+// ====================================================================
+//starting express
+// ====================================================================
+app.listen(PORT, function(err) {
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+});
